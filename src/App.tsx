b8de0bf7b816858e471421ca5f701be3322664fe
pilgrix-ux <pilgrix@gmail.com>
2026-08-18 04:49:40 +0100

    chore: align app shell background

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import { Studio } from '@/pages/Studio'
 import './App.css'
 
 function App(): JSX.Element {
-  return (
-    <div className="min-h-screen bg-slate-50">
-      <Studio />
-    </div>
-  )
+  return <Studio />
 }
 
 export default App
