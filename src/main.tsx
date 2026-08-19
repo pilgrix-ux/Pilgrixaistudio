5839f00d2b4b740bd86d0ddde795a8263cd3d04a
pilgrix-ux <pilgrix@gmail.com>
2026-08-19 20:49:25 +0100

    feat: wire AI media attachment picker

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
+import './styles/attachment-picker.css'
+import './attachment-picker'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
